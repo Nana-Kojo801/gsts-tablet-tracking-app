5d1d85d837af2c682ed62c59e93f39b498521d22
Nana-Kojo801 <124890914+Nana-Kojo801@users.noreply.github.com>
2025-07-29 11:59:33 +0000

    fix: Fixed by class submission

--- src/routes/_app/submissions/-components/class-form.tsx
+++ src/routes/_app/submissions/-components/class-form.tsx
@@ -77,7 +77,7 @@ const ClassForm = ({ closeDialog }: ClassFormProps) => {
 
   // For each student with a tablet, check if they have submitted today
   const studentsWhoHaveNotSubmitted = studentsWithTablet
-    .filter((s) => s.tablet && isConfiscatedTablet(confiscations, s))
+    .filter((s) => s.tablet && !isConfiscatedTablet(confiscations, s))
     .filter(
       (student) =>
         !submissions.some(
